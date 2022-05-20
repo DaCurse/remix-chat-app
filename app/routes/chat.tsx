95104ec26e30233f843ea506833527d7e173c555
DaCurse <dacurse0@gmail.com>
2022-05-20 21:15:31 +0300

    Added live user count and list, fixed some bugs

--- app/routes/chat.tsx
+++ app/routes/chat.tsx
@@ -1,31 +1,53 @@
 import type { ActionFunction, LoaderFunction } from '@remix-run/node'
-import { Form, useTransition } from '@remix-run/react'
+import { json, redirect } from '@remix-run/node'
+import { Form, useLoaderData, useTransition } from '@remix-run/react'
 import { useEffect, useRef, useState } from 'react'
 import type { ChatMessage } from '~/chat'
-import { getSessionUser, sendMessage } from '~/chat.server'
+import { getSessionUser, getUsers, sendMessage } from '~/chat.server'
+import { destroySession, getSession } from '~/session.server'
 
 const MAX_MESSAGE_LENGTH = 256
 
+interface LoaderData {
+  user: string
+  users: string[]
+}
+
 export const loader: LoaderFunction = async ({ request }) => {
-  return await getSessionUser(request)
+  const user = await getSessionUser(request)
+  return json<LoaderData>({ user, users: getUsers() })
 }
 
 export const action: ActionFunction = async ({ request }) => {
   const user = await getSessionUser(request)
   const formData = await request.formData()
+  const action = String(formData.get('_action'))
 
-  const message = String(formData.get('message')).slice(0, MAX_MESSAGE_LENGTH)
-  if (message.length > 0) {
-    sendMessage(user, message)
+  if (action === 'logout') {
+    const session = await getSession(request.headers.get('Cookie'))
+    return redirect('/', {
+      headers: { 'Set-Cookie': await destroySession(session) },
+    })
+  }
+
+  if (action === 'send-message') {
+    const message = String(formData.get('message')).slice(0, MAX_MESSAGE_LENGTH)
+    if (message.length > 0) {
+      sendMessage(user, message)
+    }
   }
 
   return null
 }
 
 export default function Chat() {
+  const loaderData = useLoaderData<LoaderData>()
   const transition = useTransition()
   const formRef = useRef<HTMLFormElement>(null)
   const [messages, setMessages] = useState<ChatMessage[]>([])
+  const [users, setUsers] = useState<Set<string>>(
+    () => new Set(loaderData.users)
+  )
 
   useEffect(() => {
     if (transition.state === 'submitting') {
@@ -45,16 +67,20 @@ export default function Chat() {
     })
 
     eventSource.addEventListener('user-joined', event => {
+      const user = event.data
+      setUsers(users => new Set([...users, user]))
       setMessages(messages => [
         ...messages,
-        { user: 'System', message: `"${event.data}" joined the chat` },
+        { user: 'System', message: `"${user}" joined the chat` },
       ])
     })
 
     eventSource.addEventListener('user-left', event => {
+      const user = event.data
+      setUsers(users => new Set([...users].filter(u => u !== user)))
       setMessages(messages => [
         ...messages,
-        { user: 'System', message: `"${event.data}" left the chat` },
+        { user: 'System', message: `"${user}" left the chat` },
       ])
     })
     return () => eventSource.close()
@@ -62,19 +88,43 @@ export default function Chat() {
 
   return (
     <main style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
-      <h1>Remix Chat</h1>
-      <ul>
-        {messages.map(({ user, message }, index) => (
-          <li key={index}>
-            <strong>{user}: </strong>
-            {message}
-          </li>
-        ))}
-      </ul>
-      <Form ref={formRef} method="post" replace>
-        <input type="text" name="message" />
-        <button type="submit">Send</button>
-      </Form>
+      <header style={{ marginBlock: '1rem' }}>
+        <h1 style={{ marginBlock: '0' }}>Remix Chat</h1>
+        <Form method="post">
+          <button
+            type="submit"
+            name="_action"
+            value="logout"
+            title={`${loaderData.user}, log out`}
+          >
+            Logout
+          </button>
+        </Form>
+      </header>
+      <section>
+        <div>
+          Logged in as <strong>{loaderData.user}</strong>
+        </div>
+        <div title={`Users: ${[...users].join(', ')}`}>
+          <strong>{users.size}</strong> Logged in users
+        </div>
+      </section>
+      <section>
+        <ul>
+          {messages.map(({ user, message }, index) => (
+            <li key={index}>
+              <strong>{user}: </strong>
+              {message}
+            </li>
+          ))}
+        </ul>
+        <Form ref={formRef} method="post" replace>
+          <input type="text" name="message" />
+          <button type="submit" name="_action" value="send-message">
+            Send
+          </button>
+        </Form>
+      </section>
     </main>
   )
 }
